d44ecf77114364df1079eaf40e8730e3f4122779
junebullet <67077972+junebullet@users.noreply.github.com>
2020-10-06 22:09:45 -0400

    new js, larger font on about, add text class on index

--- js/script.js
+++ js/script.js
@@ -1,18 +1,6 @@
 /*
-var flyerPopUp = document.querySelectorAll(".flyerinfo");
-var flyerText = document.querySelectorAll(".flyerinfo div");
-
-var i;
-
-for (i = 0; i < flyerPopUp.length; i++) {
-  flyerText[i].onclick = function () {
-    this.classList.toggle("show");
-  };
-}
-*/
-
 var flyerPopUp = document.querySelectorAll(".flyerinfo img");
-/*var flyerText = document.querySelectorAll(".text");*/
+var flyerText = document.querySelectorAll(".text");
 
 var i;
 
@@ -20,4 +8,26 @@ for (i = 0; i < flyerPopUp.length; i++) {
   flyerPopUp[i].onclick = function () {
     this.classList.toggle("reveal");
   };
+}
+*/
+
+var flyerPopUp = document.querySelectorAll(".flyerinfo img");
+var flyerText = document.querySelectorAll(".text");
+
+var i;
+
+for (i = 0; i < flyerPopUp.length; i++) {
+  flyerPopUp[i].onclick = function (event) {
+    showText(event.target, event.target.nextElementSibling);
+  };
+}
+
+function showText(flyerPopUp, flyerText) {
+  flyerPopUp.classList.toggle("reveal");
+  if (flyerPopUp.classList.contains("reveal")) {
+    flyerText.style.display = "block";
+  } else {
+    flyerPopUp.style.padding = "";
+    flyerText.style.display = "none";
+  }
 }
