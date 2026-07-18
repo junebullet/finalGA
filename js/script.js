e7b9440887b23fb57b8773224d1e70f7cd6e29b1
junebullet <67077972+junebullet@users.noreply.github.com>
2026-07-18 13:16:26 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,4 @@
-/*
-var flyerPopUp = document.querySelectorAll(".flyerinfo img");
-var flyerText = document.querySelectorAll(".text");
 
-var i;
-
-for (i = 0; i < flyerPopUp.length; i++) {
-  flyerPopUp[i].onclick = function () {
-    this.classList.toggle("reveal");
-  };
-}
-*/
-
-/*
-var flyerPopUp = document.querySelectorAll(".flyerinfo img");
-var flyerText = document.querySelectorAll(".text");
-
-var i;
-
-for (i = 0; i < flyerPopUp.length; i++) {
-  flyerPopUp[i].onclick = function (event) {
-    showText(event.target, event.target.nextElementSibling);
-  };
-}
-
-function showText(flyerPopUp, flyerText) {
-  flyerPopUp.classList.toggle("reveal");
-  if (flyerPopUp.classList.contains("reveal")) {
-    flyerText.style.display = "block";
-  } else {
-    flyerPopUp.style.padding = "";
-    flyerText.style.display = "none";
-  }
-}
-
-*/
 
 var flyers = document.querySelectorAll(".flyerinfo img");
 
